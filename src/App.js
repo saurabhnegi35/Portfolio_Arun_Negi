d5812d7be8bb103da7dd215e3d5183e30b552b42
SAURABH NEGI <saurabhnegi.35@gmail.com>
2023-04-25 17:54:41 +0530

    Updated Navbar Components

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
-import logo from './logo.svg';
-import GFG from './GFG_black.svg'
-import './App.css';
+// import logo from "./logo.svg";
+// import GFG from "./GFG_black.svg";
+import "./App.css";
 // import Header from './components/navbar/header';
-import Header from './components/navbar/Header';
-import Intro from './components/Intro/Intro';
-import Footer from './components/footer/Footer'
+import Header from "./components/navbar/Header";
+import Intro from "./components/Intro/Intro";
+import Footer from "./components/footer/Footer";
+import About from "./components/about/About";
+import Contact from "./components/contact/Contact";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Header/>
-      <Intro />
+      <Header />
+
+  
+        <Routes>
+          <Route path="/" element={<Intro />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          {/* <Route path='/' element={<Intro />} /> */}
+        </Routes>
+
       <Footer />
+      {/* <img src={logo} /> */}
     </div>
   );
 }
