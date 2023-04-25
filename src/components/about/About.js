9fe7db94f4d859e908568a52c5874e586d572ac4
SAURABH NEGI <saurabhnegi.35@gmail.com>
2023-04-25 17:47:36 +0530

    Added About Section Component

---
+++ src/components/about/About.js
@@ -0,0 +1,13 @@
+import './About.css'
+
+const About = () => {
+    return (
+        <>
+            <div className="about-container">
+                About
+            </div>
+        </>
+    )
+}
+
+export default About;
